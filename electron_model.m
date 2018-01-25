d575a263f2f1b147bca0c2d5153a16eab4926d85
Cameron Macarthur <cameronmacarthur@AD.DOE.CARLETON.CA>
2018-01-25 16:49:54 -0500

    PA 2

---
+++ electron_model.m
@@ -0,0 +1,38 @@
+clear
+tstep=1;
+tstop=10000
+t=0
+a=1
+x=0
+v=0
+v_arr=[]
+subplot(3,1,1)
+while t<tstop
+    xp=x
+    x=x+v
+    vp=v
+    if rand()<0.05
+        v=0
+    else 
+        v=v+a
+    end
+    v_arr=[v_arr,v]
+    vdrift=x/t
+    hold on
+    subplot(3,1,1); 
+    ylim([0 50])
+    plot(t,vdrift,'*')
+    hold on
+    subplot(3,1,2); plot(t,mean(v_arr),'*')
+   
+    
+    
+    
+    hold on
+    subplot(3,1,3)
+    plot([x,xp],[v,vp],'r')
+    pause(0.001)
+    
+    t=t+tstep
+end
+vdrift=x/tstop
